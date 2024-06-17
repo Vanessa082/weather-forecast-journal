4f0ba1c1fb0e147df9a048a311971ad07761fa45
vanessa082 <wahvanessa22@gmail.com>
2024-06-17 16:18:00 +0100

    edited the index rouste page and deleted the user

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+import pool from "../config/dbconfig.js"
+import { Router } from 'express';
 
+const router =  Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// POST /entries: Create a new journal entry with automatic weather fetching.
+// GET /entries: Retrieve all journal entries.
+// GET /entries/:id: Retrieve a single entry by its ID.
+// PUT /entries/:id: Update an existing entry.
+// DELETE /entries/:id: Delete an entry.
+
 
 module.exports = router;
