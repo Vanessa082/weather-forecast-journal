912c2925b5d155e41e67cde2736845e3ef5a6762
vanessa082 <wahvanessa22@gmail.com>
2024-06-17 16:08:26 +0100

    connected database, installing the pg dependency and the dotenv ingored the env file i use to fetch database secure information

---
+++ config/dbconfig.js
@@ -0,0 +1,18 @@
+import pg from "pg";
+import dotenv from "dotenv"
+dotenv, config();
+const { Pool } = pg;
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+pool.connect()
+.then(() => console.log("Connected to the database on port 5432"))
+.catch((err) => console.error(err))
+
+export default pool;
