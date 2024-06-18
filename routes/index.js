4a43dbd0f1433d379b4eff9690f91e899b5cdd12
vanessa082 <wahvanessa22@gmail.com>
2024-06-18 19:14:01 +0100

    GET /entries/:id: Retrieve a single entry by its ID.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 import pool from "../config/dbconfig.js"
 import { Router } from 'express';
+import validateIdParams from "../utils/validateIdParam.js"
 
 const router = Router();
 
@@ -21,9 +22,31 @@ router.get('/', function (req, res, next) {
   }
 });
 
+// GET /entries/:id: Retrieve a single entry by its ID.
+
+router.get("/:id", function (req, res, next) {
+  const { id } = req.params;
+  try {
+    validateIdParams(+id);
+    pool.query(`SELECT * FROM journal_entries WHERE id=$1;`, [id], (err, result) => {
+      if (err) next(err);
+      if (result.rowCount === 1) {
+        res.json(result.rows[0]);
+      } else {
+        const err = new Error("Entries does not exist");
+        err.status = 404;
+        next(err);
+      }
+    })
+    
+  } catch (error) {
+    next(error);
+  }
+})
+
 // POST /entries: Create a new journal entry with automatic weather fetching.
 
-// GET /entries/:id: Retrieve a single entry by its ID.
+
 // PUT /entries/:id: Update an existing entry.
 // DELETE /entries/:id: Delete an entry.
 
