6219931633ee01fd2e87574dec9ec0256ed6c32c
vanessa082 <wahvanessa22@gmail.com>
2024-06-21 19:53:05 +0100

    Send the journal entry to the backend

--- weatther_forcast_journal/src/App.jsx
+++ weatther_forcast_journal/src/App.jsx
@@ -1,35 +1,105 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import e from "express";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [date, setDate] = useState("");
+  const [description, setDescription] = useState("");
+  const [weatherCondition, setWeatherCondition] = useState("");
+  const [temperature, setTemperature] = useState("");
+  const [location, setLocation] = useState("");
+  const [longitude, setLongitude] = useState(null);
+  const [latitude, setLatitude] = useState(null);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setLongitude(position.coords.longitude);
+        setLatitude(position.coords.latitude);
+      });
+    } else {
+      alert("browser doesnot support");
+    }
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefaultt();
+    const location = `${longitude},${latitude}`;
+
+    const journalEntry = {
+      entry_date,
+      description,
+      temperature,
+      weatherCondition,
+      location,
+    };
+  };
+
+  const response = await fetch("http://localhost:3000/api/journal", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(journalEntry),
+  });
+
+  if (response.ok) {
+    console.log("Entry added successfully");
+  } else {
+    console.error("Error adding entry");
+  }
+};
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="date"
+            placeholder="YYYY-MM-DD"
+            value={date}
+            onChange={(e) => {
+              setDate(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Describe the day"
+            value={description}
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Weather condition"
+            value={weatherCondition}
+            onChange={(e) => {
+              setWeatherCondition(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Temperature"
+            value={temperature}
+            onChange={(e) => {
+              setTemperature(e.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Location"
+            value={location}
+            onChange={(e) => {
+              setLocation(e.target.value);
+            }}
+          />
+          <button type="submit">Add Entry</button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Weather Forcast Journal</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
